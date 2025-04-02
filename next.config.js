471ff76bcfe844de366fae792a852111171a7188
George Roots <squish.roots@gmail.com>
2025-04-02 18:31:18 -0400

    handling manual scaling and 308 redirects

--- next.config.js
+++ next.config.js
@@ -7,14 +7,14 @@ const nextConfig = {
   distDir: '.next',
   generateEtags: false,
   poweredByHeader: false,
-  trailingSlash: true,
+  trailingSlash: false,
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: process.env.NODE_ENV === 'production'
-          ? 'https://meeting-spot-backend-270814322595.us-east1.run.app/api/:path*/'
-          : 'http://localhost:3001/api/:path*/',
+          ? 'https://meeting-spot-backend-270814322595.us-east1.run.app/api/:path*'
+          : 'http://localhost:3001/api/:path*',
       },
     ]
   },
