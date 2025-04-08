251b28c4dcb42e916123e0c98c288f068178fe51
George Roots <squish.roots@gmail.com>
2025-04-08 13:29:53 -0400

    Fix layout.tsx metadata export

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
